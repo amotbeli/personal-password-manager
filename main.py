797c861d9a982e8897f26f06b7ca75966e40e2b6
amotbeli <amotbeli@protonmail.com>
2024-06-09 08:40:05 +0530

    Add functionality to generate random username

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("hello world")
+import random
 
+def main():
     with open('adjectives.txt', 'r') as file:
         adjectives = file.readlines()
         adjectives = [adjective.strip() for adjective in adjectives]
@@ -9,8 +9,14 @@ def main():
         nouns = file.readlines()
         nouns = [noun.strip() for noun in nouns]
 
-    print(adjectives)
-    print(nouns)
+    random.shuffle(adjectives)
+    random.shuffle(nouns)
+
+    adjective_index = random.randint(0, len(adjectives) - 1)
+    noun_index = random.randint(0, len(nouns) - 1)
+
+    random_username = adjectives[adjective_index] + "_" + nouns[noun_index]
+    print(random_username)
 
 if __name__ == "__main__":
     main()
